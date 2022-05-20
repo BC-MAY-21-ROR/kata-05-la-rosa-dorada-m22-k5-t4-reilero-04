d48294a0f8752be43ce45e3c845defcce2f98e9b
AgustinPalmaM <palma.agustin7@gmail.com>
2022-05-20 18:48:30 -0400

    Implement test for the legacy code and confirm class and methods are running ok
    Co-authored-by: acorzo <alexecorzo@gmail.com>
    Co-authored-by: HikingCarrot7 <ricardoibarra2044@gmail.com>

---
+++ spec/gilded_rose_spec.rb
@@ -0,0 +1,83 @@
+# require File.join(File.dirname(__FILE__), 'gilded_rose')
+require 'gilded_rose'
+
+describe GildedRose do
+  describe '#update_quality' do
+    it 'does not change the name' do
+      items = [Item.new('foo', 0, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].name).to eq 'foo'
+    end
+
+    it ' item is Aged brie' do
+      items = [Item.new('Aged Brie', 5, 10)]
+      GildedRose.new(items).update_quality
+      expect(items[0].sell_in).to eq(4)
+    end
+
+    it ' item is Aged brie quality after one day' do
+      items = [Item.new('Aged Brie', 5, 10)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(11)
+    end
+
+    it ' item is Aged brie quality when sell_in is 0' do
+      items = [Item.new('Aged Brie', 0, 10)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(12)
+    end
+
+    it ' item is Aged brie quality when sell_in is under 0' do
+      items = [Item.new('Aged Brie', -1, 10)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(12)
+    end
+
+    it ' item when item has quality 50' do
+      items = [Item.new('Aged Brie', -1, 50)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(50)
+    end
+
+    # Testing another item instead Aged Brie
+    it ' item quality never it will be less than 0' do
+      items = [Item.new('Foo', -1, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(0)
+    end
+
+    it ' Items sell_in > 0, Quality degrades once ' do
+      items = [Item.new('Foo', 2, 4)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(3)
+    end
+
+    it ' Once the sell by date has passed, Quality degrades twice as fast ' do
+      items = [Item.new('Foo', -1, 4)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(2)
+    end
+
+    # Testing item Backstage passes
+    it ' backstage increase quality by 2 when sell_in is less or equal than 10 ' do
+      backstage = 'Backstage passes to a TAFKAL80ETC concert'
+      items = [Item.new(backstage, 10, 4)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(6)
+    end
+
+    it ' backstage increase quality by 3 when sell_in is less or equal than 5 ' do
+      backstage = 'Backstage passes to a TAFKAL80ETC concert'
+      items = [Item.new(backstage, 5, 4)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(7)
+    end
+
+    it ' backstage reset quality to 0 after the concert ' do
+      backstage = 'Backstage passes to a TAFKAL80ETC concert'
+      items = [Item.new(backstage, 0, 4)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(0)
+    end
+  end
+end
